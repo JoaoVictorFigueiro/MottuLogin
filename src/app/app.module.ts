a86585fbf43bdca10a02f162fc21454cfd1d26b9
João Victgor Figueiró <87573308+JoaoVictorFigueiro@users.noreply.github.com>
2021-07-18 16:40:43 -0300

    primeiro comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './review/login/login.component';
+import {CardModule} from 'primeng/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {PasswordModule} from 'primeng/password';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    CardModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
