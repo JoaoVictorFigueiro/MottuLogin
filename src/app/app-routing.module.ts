20371c82c420452f866f91252a4f285b719bb22c
GuiPai <84127487+GuiMariano@users.noreply.github.com>
2021-07-21 19:58:52 -0300

    Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { LoginComponent } from './review/login/login.component';
 import { TabelaComponent } from './tabela/tabela.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'tabela', component: TabelaComponent}
+  {path: 'tabela', component: TabelaComponent},
+  {path: 'cadastro', component: CadastroComponent}
 ];
 
 @NgModule({
