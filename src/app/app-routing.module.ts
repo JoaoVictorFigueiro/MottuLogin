73d3aef8512b640ffc1f45b73b30cb47356ce2ee
GuiPai <84127487+GuiMariano@users.noreply.github.com>
2021-07-25 19:20:12 -0300

    Tela Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './review/login/login.component';
 import { TabelaComponent } from './tabela/tabela.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'tabela', component: TabelaComponent},
-  {path: 'cadastro', component: CadastroComponent}
+  {path: 'cadastro', component: CadastroComponent},
+  {path:'home', component: HomeComponent}
 ];
 
 @NgModule({
